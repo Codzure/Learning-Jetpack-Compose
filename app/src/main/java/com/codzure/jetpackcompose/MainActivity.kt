352b86436430e4ead92f6ed558b2aca9a9470b14
Leonard Mutugi <leonard.mutugi@equitybank.co.ke>
2023-09-19 15:36:59 +0300

    first commit

---
+++ app/src/main/java/com/codzure/jetpackcompose/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.codzure.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.codzure.jetpackcompose.ui.theme.JetpackComposeLearningTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeLearningTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                    border = null,
+                    shadowElevation = 10 dip
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeLearningTheme {
+        Greeting("Android")
+    }
+}
